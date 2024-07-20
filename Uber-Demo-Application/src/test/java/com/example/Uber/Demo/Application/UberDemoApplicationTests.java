22ab80c94b3c063f0cbeac64e6ff9188c7e16bb6
Sakxam Shrestha <143928022+SakxamShrestha@users.noreply.github.com>
2024-07-20 04:38:59 -0500

    pushed by Sakxam

---
+++ Uber-Demo-Application/src/test/java/com/example/Uber/Demo/Application/UberDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Uber.Demo.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
